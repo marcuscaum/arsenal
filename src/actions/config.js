4b3746ca577a0b9d0744d7639641ed4bcf832a95
Marcus Caum <marcuslucas.jb@gmail.com>
2017-08-26 22:05:04 -0300

    Fix list images

--- src/actions/config.js
+++ src/actions/config.js
@@ -7,7 +7,7 @@ import {
 
 export const Api = new Kindred({
   key: 'RGAPI-73be25fa-d728-4e84-99fd-b5b8cf9535cf',
-  defaultRegion: REGIONS.BRAZIL,
+  defaultRegion: REGIONS.TURKEY,
   limits: LIMITS.DEV, // [[20, 1], [100, 120]] 20 reqs/1s. 100 reqs/2m.
   debug: true,
   showHeaders: true,
--- src/shared/components/List/index.jsx
+++ src/shared/components/List/index.jsx
@@ -3,11 +3,17 @@ import _ from 'underscore';
 
 import './index.css';
 
+const emptyImageHandler = (image, cdn) => {
+  return fetch(`${cdn + image}`)
+    .then(data => data.status === 404)
+}
+
+
 export const List = props => (
   <div className='list'>
     {_.map(props.items, (item, key) => (
-      <div className={`list-item ${props.type}`} key={key} style={{ backgroundImage: `url(${props.images + item.image.full})`}}>
-        {item.name}
+      <div className={`list-item ${props.type}`} key={key}>
+        <img src={`${props.images + item.image.full}`} onError={(e) => e.target.src=`http://ddragon.leagueoflegends.com/cdn/img/champion/loading/${item.name}_0.jpg`}/>
       </div>
     ))}
   </div>
